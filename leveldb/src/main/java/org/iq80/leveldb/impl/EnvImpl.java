486e78fd32859b9b0340e342a97606306db11fba
Honoré Vasconcelos <honore.vasconcelos@gmail.com>
2018-01-24 23:17:00 +0000

    Improve control over number of opened memory mapped files

--- leveldb/src/main/java/org/iq80/leveldb/impl/EnvImpl.java
+++ leveldb/src/main/java/org/iq80/leveldb/impl/EnvImpl.java
@@ -22,25 +22,34 @@ import org.iq80.leveldb.util.MMWritableFile;
 import org.iq80.leveldb.util.RandomInputFile;
 import org.iq80.leveldb.util.SequentialFile;
 import org.iq80.leveldb.util.SequentialFileImpl;
+import org.iq80.leveldb.util.Slice;
 import org.iq80.leveldb.util.UnbufferedRandomInputFile;
 import org.iq80.leveldb.util.UnbufferedWritableFile;
 import org.iq80.leveldb.util.WritableFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.concurrent.TimeUnit;
 
 public class EnvImpl implements Env
 {
     private static final int PAGE_SIZE = 1024 * 1024;
+    private final MmapLimiter mmapLimiter;
 
-    private EnvImpl()
+    private EnvImpl(MmapLimiter mmapLimiter)
     {
+        this.mmapLimiter = mmapLimiter;
     }
 
     public static Env createEnv()
     {
-        return new EnvImpl();
+        return new EnvImpl(MmapLimiter.defaultLimiter());
+    }
+
+    public static Env createEnv(MmapLimiter mmapLimiter)
+    {
+        return new EnvImpl(mmapLimiter);
     }
 
     @Override
@@ -58,18 +67,106 @@ public class EnvImpl implements Env
     @Override
     public RandomInputFile newRandomAccessFile(File file) throws IOException
     {
-        return Iq80DBFactory.USE_MMAP ? MMRandomInputFile.open(file) : UnbufferedRandomInputFile.open(file);
+        if (mmapLimiter.acquire()) {
+            try {
+                return new DelegateRandomInputFile(mmapLimiter, MMRandomInputFile.open(file));
+            }
+            catch (IOException e) {
+                mmapLimiter.release();
+                throw e;
+            }
+        }
+        return UnbufferedRandomInputFile.open(file);
     }
 
     @Override
     public WritableFile newWritableFile(File file) throws IOException
     {
-        return Iq80DBFactory.USE_MMAP ? MMWritableFile.open(file, PAGE_SIZE) : UnbufferedWritableFile.open(file);
+        if (mmapLimiter.acquire()) {
+            try {
+                return new DelegateWritableFile(mmapLimiter, MMWritableFile.open(file, PAGE_SIZE));
+            }
+            catch (IOException e) {
+                mmapLimiter.release();
+                throw e;
+            }
+        }
+        return UnbufferedWritableFile.open(file);
     }
 
     @Override
     public WritableFile newAppendableFile(File file) throws IOException
     {
         return UnbufferedWritableFile.open(file);
+    }
+
+    private static class DelegateRandomInputFile implements RandomInputFile
+    {
+        private final MmapLimiter mmapLimiter;
+        private final RandomInputFile open;
+
+        DelegateRandomInputFile(MmapLimiter mmapLimiter, RandomInputFile open)
+        {
+            this.mmapLimiter = mmapLimiter;
+            this.open = open;
+        }
+
+        @Override
+        public long size()
+        {
+            return open.size();
+        }
+
+        @Override
+        public ByteBuffer read(long offset, int length) throws IOException
+        {
+            return open.read(offset, length);
+        }
+
+        @Override
+        public void close() throws IOException
+        {
+            try {
+                open.close();
+            }
+            finally {
+                mmapLimiter.release();
+            }
+        }
+    }
+
+    private static class DelegateWritableFile implements WritableFile
+    {
+        private final MmapLimiter mmapLimiter;
+        private final WritableFile open;
+
+        DelegateWritableFile(MmapLimiter mmapLimiter, WritableFile open)
+        {
+            this.mmapLimiter = mmapLimiter;
+            this.open = open;
+        }
+
+        @Override
+        public void append(Slice data) throws IOException
+        {
+            open.append(data);
+        }
+
+        @Override
+        public void force() throws IOException
+        {
+            open.force();
+        }
+
+        @Override
+        public void close() throws IOException
+        {
+            try {
+                open.close();
+            }
+            finally {
+                mmapLimiter.release();
+            }
+        }
     }
 }
--- leveldb/src/main/java/org/iq80/leveldb/impl/Iq80DBFactory.java
+++ leveldb/src/main/java/org/iq80/leveldb/impl/Iq80DBFactory.java
@@ -36,24 +36,6 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 public class Iq80DBFactory
         implements DBFactory
 {
-    public static final int CPU_DATA_MODEL;
-
-    static {
-        boolean is64bit;
-        if (System.getProperty("os.name").contains("Windows")) {
-            is64bit = System.getenv("ProgramFiles(x86)") != null;
-        }
-        else {
-            is64bit = System.getProperty("os.arch").contains("64");
-        }
-        CPU_DATA_MODEL = is64bit ? 64 : 32;
-    }
-
-    // We only use MMAP on 64 bit systems since it's really easy to run out of
-    // virtual address space on a 32 bit system when all the data is getting mapped
-    // into memory.  If you really want to use MMAP anyways, use -Dleveldb.mmap=true
-    public static final boolean USE_MMAP = Boolean.parseBoolean(System.getProperty("leveldb.mmap", "" + (CPU_DATA_MODEL > 32)));
-
     public static final String VERSION;
 
     static {
